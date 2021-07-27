656754c8858da709bd3e9a6da2b889d9893143ae
Haris Musić <reha-1921@hotmail.com>
2021-07-28 00:43:30 +0200

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import ProfileFeedItem, UserProfile
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,8 @@ router.register('hello-viewset',views.HelloViewSet,'hello-viewset')
 
 router.register('profile',views.UserProfileViewSet)
 
+router.register('feed',views.UserProfileFeedViewSet)
+
 urlpatterns = [
     
     path('hello-view/', views.HelloApiView.as_view()),
